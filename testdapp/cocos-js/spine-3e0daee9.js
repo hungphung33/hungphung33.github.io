73169f09050f0d1643029258ac0d99bbf983d176
Phùng Việt Hưng <hungphung33@gmail.com>
2024-08-02 12:00:03 +0700

    test up

--- testdapp/cocos-js/spine-3e0daee9.js
+++ testdapp/cocos-js/spine-3e0daee9.js
@@ -1,1 +1,11 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine-f02329b9.wasm")}}}));
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var spine = exports('default', 'assets/spine-f02329b9.wasm');
+
+		})
+	};
+}));
+//# sourceMappingURL=spine-3e0daee9.js.map
--- testdapp/cocos-js/spine.js-f7f5ea79.js
+++ testdapp/cocos-js/spine.js-f7f5ea79.js
@@ -1,1 +1,11 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine.js.mem-1ff7a7d3.bin")}}}));
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var spine_js = exports('default', 'assets/spine.js.mem-1ff7a7d3.bin');
+
+		})
+	};
+}));
+//# sourceMappingURL=spine.js-f7f5ea79.js.map
